08e689a88dfe82ab5e53cf43d6177678ecc87d89
aelys <aelysch@proton.me>
2024-04-20 00:38:12 -0300

    Update background image, title, and input styling

--- src/components/Search_Button.tsx
+++ src/components/Search_Button.tsx
@@ -5,7 +5,7 @@ export const SearchButton = () => {
   return (
     <div className='flex'>
      
-      <button value="Search" className='shadow-2xl mt-1 p-2 bg-red-200 border rounded-md focus:border-gray-200 duration-300' >
+      <button value="Search" className='shadow-2xl mt-1 p-3 bg-red-200 border rounded-md focus:border-gray-200 duration-300' >
         <span>
           <CiSearch></CiSearch>
         </span>
--- src/components/Search_Input.tsx
+++ src/components/Search_Input.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SearchInput = () => {
   return (
     <div className=''>
-        <input type='text' placeholder='Search' className='shadow-2xl font-extralight mt-1 p-2 text-sm w-full border rounded-md focus:border-gray-200 duration-300'></input>
+        <input type='text' placeholder='Search' className='shadow-2xl font-extralight mt-1 p-3 text-sm w-full border rounded-md focus:border-gray-200 duration-300'></input>
     </div>
   )
 }
