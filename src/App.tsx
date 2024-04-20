d2e7a3a6f1f3550f7be6736631c4f2a99a89544f
aelys <aelysch@proton.me>
2024-04-20 00:33:32 -0300

    Update background image and footer styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ export default function App () {
 
 
   return (
-    <>
-    <div  className="h-screen w-screen font-Inter bg-fixed bg-cover pt-20 bg-[url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6d66e85e-f832-4fe9-b642-1ecddde1a3a9/dg1vi34-67d81e1a-678a-4afc-acdd-91761b20ed62.png/v1/fill/w_1192,h_670,q_70,strp/nature___from_up_on_poppy_hill_by_tsuyoiiii_dg1vi34-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNmQ2NmU4NWUtZjgzMi00ZmU5LWI2NDItMWVjZGRkZTFhM2E5XC9kZzF2aTM0LTY3ZDgxZTFhLTY3OGEtNGFmYy1hY2RkLTkxNzYxYjIwZWQ2Mi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.9IQi1TeVzj_br8nsFndKFL6P3_1MfkL8Vny-EZ5nln8')]">
+    <div className='h-screen'>
+    <div  className="h-screen font-Inter bg-fixed bg-cover pt-20 bg-[url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6d66e85e-f832-4fe9-b642-1ecddde1a3a9/dg1vi34-67d81e1a-678a-4afc-acdd-91761b20ed62.png/v1/fill/w_1192,h_670,q_70,strp/nature___from_up_on_poppy_hill_by_tsuyoiiii_dg1vi34-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNmQ2NmU4NWUtZjgzMi00ZmU5LWI2NDItMWVjZGRkZTFhM2E5XC9kZzF2aTM0LTY3ZDgxZTFhLTY3OGEtNGFmYy1hY2RkLTkxNzYxYjIwZWQ2Mi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.9IQi1TeVzj_br8nsFndKFL6P3_1MfkL8Vny-EZ5nln8')]">
       <div className='backdrop-blur-xl shadow-2xl shadow-black/50 mx-auto w-fit p-6 rounded-2xl'>
       <div className=" text-center pt-5">
         <h1 className="underline decoration-sky-500 text-2xl bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent font-light pb-1">Discord ID Lookup</h1>
@@ -26,7 +26,7 @@ export default function App () {
      
     </div>
     <Footer></Footer>
-    </>
+    </div>
   );
 };
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-stone-800 font-Inter justify-between flex h-20'>
+    <div className='bg-stone-800 w-full font-Inter justify-between flex h-20'>
         <div className='flex gap-24 items-center mx-auto justify-between p-4'>
            <a className='text-white font-extralight text-xs'>Made by  <span className='text-rose-400'>Aelys</span> with ❤️</a>
            <a className='text-white font-extralight text-xs underline hover:text-gray-400 duration-300' href="">Github</a>
