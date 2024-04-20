1dfc68b86d046e729fd227ee09b962c7e8ab4a32
aelys <aelysch@proton.me>
2024-04-20 10:30:34 -0300

    Update User type properties to be optional

--- src/types/User.ts
+++ src/types/User.ts
@@ -3,11 +3,11 @@ export type Avatar = {
 }
 
 export type User = {
-    username: string | undefined,
-    avatar: Avatar | undefined,
-    badges: string[] | undefined,
-    accent_color: string | undefined,
-    global_name: string | undefined,
-    banner_link: string | null | undefined,
-    banner_color: string | undefined
+    username?: string | undefined,
+    avatar?: Avatar | undefined,
+    badges?: string[] | undefined,
+    accent_color?: string | undefined,
+    global_name?: string | undefined,
+    banner_link?: string | null | undefined,
+    banner_color?: string | undefined
 }
